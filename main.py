f1b3e4485ebf2f9c116007c0230fb7176a515bfd
Nimesh Srivastava <snimesh412@gmail.com>
2021-11-30 17:55:27 +0530

    Mail client with attachment working

--- main.py
+++ main.py
@@ -5,20 +5,36 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-server = smtplib.smtp('smtp.gmail.com', 25)
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
 
 with open('password.txt', 'r') as f:
     password = f.read()
 
-server.login('testphp29@gmail.com', password)
+server.login('andromedag90@gmail.com', password)
 
 msg = MIMEMultipart()
 msg['From'] = 'TestAccount'
-msg['To'] = 'testmail@spaml.de'
+msg['To'] = 'snimesh412@gmail.com'
 msg['Suject'] = 'Test email from python mailing client'
 
 with open('message.txt', 'r') as f:
     message = f.read()
 
 msg.attach(MIMEText(message, 'plain'))
+
+filename = 'img.jpeg'
+attachment = open(filename, 'rb')
+
+p = MIMEBase('application', 'octet-stream')
+p.set_payload(attachment.read())
+
+encoders.encode_base64(p)
+p.add_header(
+    "Content-Disposition",
+    "attachment; filename={}".format(filename),
+)
+msg.attach(p)
+
+text = msg.as_string()
+server.sendmail('andromedag90@gmail.com', 'snimesh412@gmail.com', text)
