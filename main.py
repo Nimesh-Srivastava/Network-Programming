9f80243766adf247b6131002213f17e7e92a655f
Nimesh Srivastava <snimesh412@gmail.com>
2021-11-30 15:45:23 +0530

    Add text message

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import smtplib
 
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
 server = smtplib.smtp('smtp.gmail.com', 25)
 server.ehlo()
 
@@ -7,3 +12,8 @@ with open('password.txt', 'r') as f:
     password = f.read()
 
 server.login('testphp29@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['From'] = 'TestAccount'
+msg['To'] = 'testmail@spaml.de'
+msg['Suject'] = 'Test email from python mailing client'
